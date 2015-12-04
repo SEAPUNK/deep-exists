6dd9c940a7fb395b7c0f1d6ae69437c0162d7693
Ivan <ivan@sq10.net>
2015-12-04 16:22:04 -0600

    code

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = function (obj, sub) {
+  if (sub) {
+    sub = sub.split('.')
+  }
+
+  if (obj === null || obj === undefined) return false
+
+  if (!sub) return true
+
+  var current = obj
+  var prop
+  for (var i = 0;i < sub.length;i++) {
+    prop = sub[i]
+    if (current[prop] === null || current[prop] === undefined) return false
+    current = current[prop]
+  }
+
+  return true
+}
